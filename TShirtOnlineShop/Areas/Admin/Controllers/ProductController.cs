2f11e1d89060c59246afc328815f86bf8f47fe97
drake <caongocphuong95@gmail.com>
2019-06-19 13:05:40 +0700

    commit 2

---
+++ TShirtOnlineShop/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,94 @@
+﻿using AutoMapper;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TShirtOnlineShop.Models;
+using TShirtOnlineShop.ViewModel;
+
+namespace TShirtOnlineShop.Areas.Admin.Controllers
+{
+    public class ProductController : Controller
+    {
+        private OnlineShopEntities db = new OnlineShopEntities();
+        // GET: Admin/Product
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public JsonResult Data()
+        {
+            var list = db.Products.OrderByDescending(x => x.ID).ToList();
+            return Json(Mapper.Map<List<ProductViewModel>>(list), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult ProductDetail(int id)
+        {
+            var product = db.Products.Find(id);
+            return Json(Mapper.Map<ProductViewModel>(product), JsonRequestBehavior.AllowGet);
+        }
+
+        public JsonResult AddProduct()
+        {
+            var a= UploadImage();
+            return Json(null, JsonRequestBehavior.AllowGet);
+        }
+
+
+        public string UploadImage()
+        {
+            DateTime dateTime = DateTime.Now;
+            string tick = dateTime.Ticks.ToString();
+            string fname = "";
+            string path = null; ;
+            int size = 0;
+            List<string> LIST_IMAGE_TYPE = new List<string>() { ".jpg", ".jpeg", ".gif", ".png" };
+            List<dynamic> media = new List<dynamic>();
+
+            if (Request.Files.Count > 0)
+            {
+                try
+                {
+                    //  Get all files from Request object  
+                    HttpFileCollectionBase files = Request.Files;
+                    for (int i = 0; i < files.Count; i++)
+                    {
+
+                        HttpPostedFileBase file = files[i];
+                        string extension = Path.GetExtension(file.FileName).ToLowerInvariant();
+
+                        // Checking for Internet Explorer  
+                        if (Request.Browser.Browser.ToUpper() == "IE" || Request.Browser.Browser.ToUpper() == "INTERNETEXPLORER")
+                        {
+                            string[] testfiles = file.FileName.Split(new char[] { '\\' });
+                            fname = testfiles[testfiles.Length - 1];
+                        }
+                        else
+                        {
+                            fname = file.FileName;
+                            size = file.ContentLength / 1024;
+                        }
+                        if (LIST_IMAGE_TYPE.Contains(extension))
+                        {
+                            fname = Path.Combine(Server.MapPath("~/Content/images"), tick + extension);
+                            file.SaveAs(fname);
+                            path = "/Content/images/" + tick + extension;
+                        }
+
+                        media.Add(new { url = path });
+                    }
+
+                }
+
+                catch (Exception)
+                {
+                    return path;
+                }
+
+            }
+            return path;
+        }
+    }
+}
---
+++ TShirtOnlineShop/Areas/Admin/Script/ProductController.js
@@ -0,0 +1,99 @@
+﻿(function () {
+    var app = angular.module('AppAdmin', ['angularUtils.directives.dirPagination']);
+    app.controller('ProductController', ['$scope', '$http', '$q', function ($scope, $http, $q) {
+
+        init();
+        function init() {
+                var $def = $q.defer();
+                $http.get('/Admin/Product/Data').then(function (response) {
+                    $def.resolve(response.data);
+                    $scope.listProduct = response.data;
+                    console.log($scope.listProduct);
+                }, function () {
+                    $def.reject('Error getting roles');
+                });
+                return $def.promise;
+
+        }
+
+        function ListLog(data) {
+            $scope.listProduct = data;
+        }
+
+
+        function ShowMessage(data) {
+            if (data.status === 1) {
+                $.notify(data.message, "success");
+            } else {
+                $.notify(data.message, "error");
+            }
+
+        }
+        $scope.AddProduct = function (product, m) {
+            var $def = $q.defer();
+            $http.get('/Admin/Product/AddProduct').then(function (response) {
+                $def.resolve(response.data);
+                $scope.listProduct = response.data;
+                console.log($scope.listProduct);
+            }, function () {
+                $def.reject('Error getting roles');
+            });
+            return $def.promise;
+           // $(m).modal("hide");
+            //homeService.AddLog(log).then(function (response) {
+            //   // ShowMessage(response);
+            //   // homeService.GetAllLog().then(ListLog).finally(commonService.EndLoading);
+               
+            //});
+        };
+
+        $scope.OpenModalUpdate = function (log) {
+            log.NotificationDate = new Date(log.NotificationDate);
+            log.IncidentDate = new Date(log.IncidentDate);
+            $scope.log = angular.copy(log);
+        };
+
+        $scope.OpenModalDelete = function (log) {
+            log.NotificationDate = new Date(log.NotificationDate);
+            log.IncidentDate = new Date(log.IncidentDate);
+            $scope.log = angular.copy(log);
+        };
+
+        $scope.UpdateLog = function (log, m) {
+            commonService.StartLoading();
+            homeService.UpdateLog(log).then(function (response) {
+                ShowMessage(response);
+                homeService.GetAllLog().then(ListLog).finally(commonService.EndLoading);
+                $(m).modal("hide");
+            });
+        };
+
+        $scope.DeleteLog = function (log, m) {
+            commonService.StartLoading();
+            homeService.DeleteLog(log).then(function (response) {
+                ShowMessage(response);
+                homeService.GetAllLog().then(ListLog).finally(commonService.EndLoading);
+                $(m).modal("hide");
+            });
+        };
+ 
+
+    }]).directive("fileread", [function () {
+        return {
+            scope: {
+                fileread: "="
+            },
+            link: function (scope, element, attributes) {
+                element.bind("change", function (changeEvent) {
+                    var reader = new FileReader();
+                    reader.onload = function (loadEvent) {
+                        scope.$apply(function () {
+                            scope.fileread = loadEvent.target.result;
+                        });
+                    }
+                    reader.readAsDataURL(changeEvent.target.files[0]);
+                });
+            }
+        }
+    }]);;
+})();
