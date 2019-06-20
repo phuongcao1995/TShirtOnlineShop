e3134085f7c414621bcbf6a4585421f639438ca1
drake <caongocphuong95@gmail.com>
2019-06-20 17:11:39 +0700

    search product

--- TShirtOnlineShop/Controllers/MenController.cs
+++ TShirtOnlineShop/Controllers/MenController.cs
@@ -1,4 +1,5 @@
 ﻿using AutoMapper;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web.Mvc;
@@ -30,15 +31,37 @@ namespace TShirtOnlineShop.Controllers
             ViewBag.productId = id;
             return View();
         }
-        public JsonResult Data(int? type)
+        public JsonResult Data(string size, string colors, int? price, int? type)
         {
-            var list = db.Products.Where(x => x.CategoryID == type).OrderByDescending(x => x.ID).ToList();
-            return Json(Mapper.Map<List<ProductViewModel>>(list), JsonRequestBehavior.AllowGet);
+            var list = db.Products.Where(x => x.CategoryID == type);
+
+            if (!string.IsNullOrWhiteSpace(size))
+            {
+                list = list.Where(x => x.Size.Equals(size, StringComparison.OrdinalIgnoreCase));
+            }
+            if (price != null && price == 1)
+            {
+                list = list.Where(x => x.Price < 10);
+            }
+            else if (price != null && price == 2)
+            {
+                list = list.Where(x => x.Price < 20);
+            }
+            else if (price != null && price == 3)
+            {
+                list = list.Where(x => x.Price > 20);
+            }
+            if (!string.IsNullOrWhiteSpace(colors))
+            {
+                list = list.Where(x => x.Colors.Equals(colors, StringComparison.OrdinalIgnoreCase));
+            }
+
+            return Json(Mapper.Map<List<ProductViewModel>>(list.OrderByDescending(x => x.ID)), JsonRequestBehavior.AllowGet);
         }
         public JsonResult ProductDetail(int id)
         {
             var product = db.Products.Find(id);
-            var anotherProduct = db.Products.Where(x => x.Category.Type == product.Category.Type && x.Status != false && x.ID!= id).Take(4).ToList();
+            var anotherProduct = db.Products.Where(x => x.Category.Type == product.Category.Type && x.Status != false && x.ID != id).Take(4).ToList();
             return Json(new
             {
                 product = Mapper.Map<ProductViewModel>(product),
