9fd744e5d29dedd22d3ec8d314a90c2184e0e32a
PhuongCao <caongocphuong95@gmail.com>
2021-11-17 13:29:42 +0700

    combie commit

---
+++ TShirtOnlineShop/Controllers/ShoesController.cs
@@ -0,0 +1,25 @@
+﻿using System.Web.Mvc;
+
+namespace TShirtOnlineShop.Controllers
+{
+    public class ShoesController : Controller
+    {
+        // GET: Shoes
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Update()
+        {
+            return View();
+        }
+        public ActionResult Detail()
+        {
+            return View();
+        }
+    }
+}
+
+
+
+
